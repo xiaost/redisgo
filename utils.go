09242a1de4add96a2f0dcb4580919ccc0141724f
xiaost <xiaost7@gmail.com>
2018-06-12 23:56:52 +0800

    fix invalid arg type

--- utils.go
+++ utils.go
@@ -8,7 +8,7 @@ func validarg(a interface{}) bool {
 	switch a.(type) {
 	case int, int8, int16, int32, int64:
 	case uint, uint8, uint16, uint32, uint64:
-	case float32, float64, []byte, string:
+	case float32, float64, []byte, string, []string:
 	default:
 		return false
 	}
